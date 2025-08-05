7bd04b596002967f258759365df9ce01216878b9
Emma785-sudo <emmanuelkayumbayi@gmail.com>
2025-08-05 16:49:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: "drakonis~g2NS0J5R#-6taAIDRhdUONVayKhtKG6OQS1cdK63JhFjazIRuyn8",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "243983278829", // PUT OWNER NUMBER HERE
+  PREFIX: "•" // prefix (e.g., ., /, !, *)
 };
